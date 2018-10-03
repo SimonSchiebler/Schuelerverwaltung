2275930665cc82f08bd523051f5fb09974cf376b
unknown <simonschiebler@gmail.com>
2018-10-03 15:14:57 +0200

    added fist view

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const nodeCouchDb = require('node-couchdb');
+const fs = require('fs');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 const couch = new nodeCouchDb({
-    auth:{
+    auth: {
         user: 'admin',
         password: 'admin'
     },
@@ -12,22 +15,44 @@ const couch = new nodeCouchDb({
     protocol: 'http',
 });
 
-couch.listDatabases().then(function(dbs){
+const dbname = 'schueler';
+const viewUrl = '_design/v1/_view/id'
+
+couch.listDatabases().then(function (dbs) {
     console.log(dbs);
 })
 
+
+
 const app = express();
 
-app.set('view-engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.get('/lehrer', function (req, res) {
+    const htmlSource = fs.readFileSync("views/lehrer/lehrer.html", "utf8");
+    const styles = fs.readFileSync("views/lehrer/lehrer.css", "utf8");
+    const responseView = new JSDOM(htmlSource);
+    const doc = responseView.window.document;
+    const head = doc.getElementsByTagName('head')[0];
+    const body = doc.getElementsByTagName('body')[0]
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+    // add styles to document
+    style = doc.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = styles;
+    head.appendChild(style);
 
-app.get('/', function(req, res){
-    res.send('working...')
+    couch.get('schueler', '_design/v1/_view/id').then((schueler) => {
+        const listDiv = doc.getElementById('content');
+        schueler.data.rows.forEach(scolar => {
+            let listentry = doc.createElement('p');
+            listentry.textContent = scolar.value
+            listDiv.appendChild(listentry) 
+        });  
+        res.send(responseView.serialize())    
+    });
+
+
 });
- 
-app.listen(3000, function(){
+
+app.listen(3000, function () {
     console.log('Server startet on Port 3000')
 })
