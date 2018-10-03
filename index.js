866ba6d8325a7b5b7c4e785653c15c8d3a83633f
unknown <simonschiebler@gmail.com>
2018-10-03 18:28:13 +0200

    added navbar

--- index.js
+++ index.js
@@ -18,17 +18,28 @@ const couch = new nodeCouchDb({
 const SCHUELERDB = 'schueler';
 const viewUrl = '_design/v1/_view/id'
 
-function getMainPage() {
-    const htmlSource = fs.readFileSync("views/main.html", "utf8");
-    const styles = fs.readFileSync("views/main.css", "utf8");
-    const responseView = new JSDOM(htmlSource);
-    const doc = responseView.window.document;
-    const head = doc.getElementsByTagName('head')[0];
+function addstyles (pageDom, stylepath){
+    const styles = fs.readFileSync(stylepath, 'utf8');
+    const head = pageDom.window.document.getElementsByTagName('head')[0];
 
-    style = doc.createElement('style');
+    const style = pageDom.window.document.createElement('style');
     style.type = 'text/css';
     style.innerHTML = styles;
     head.appendChild(style);
+}
+
+function getMainPage() {
+    const htmlSource = fs.readFileSync("views/main.html", "utf8");
+    const menuHTML = fs.readFileSync("views/templates/mainMenu.html", "utf8");
+    
+    const responseView = new JSDOM(htmlSource);
+    const doc = responseView.window.document;
+    addstyles(responseView, "views/main.css");
+    
+    const mainMenu = doc.getElementById('menu');
+    mainMenu.classList = "w3-sidebar w3-bar-block";
+    mainMenu.innerHTML = menuHTML;
+    addstyles(responseView, "views/templates/mainMenu.css")
 
     return responseView;
 }
@@ -50,6 +61,7 @@ app.get('/', function (req, res) {
 app.get('/lehrer', function (req, res) {
     const responseView = getMainPage(); 
     const doc = responseView.window.document;
+    addstyles(responseView, "views/templates/lehrer.css");
     const head = doc.getElementsByTagName('head')[0];
     const body = doc.getElementsByTagName('body')[0];
 
@@ -68,7 +80,7 @@ app.get('/lehrer', function (req, res) {
 
 });
 
-app.get('/neuerSchueler', function (req, res) {
+app.get('/schuelerAnlegen', function (req, res) {
 
     const responseView = getMainPage();
     const doc = responseView.window.document;
