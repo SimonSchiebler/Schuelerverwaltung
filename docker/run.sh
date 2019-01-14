a874f00b0f19956b37bb2e705fbbfdf81f06d782
simonschiebler <simonschiebler@gmail.com>
2019-01-14 11:39:20 +0100

    assigned static volume to docker container

--- docker/run.sh
+++ docker/run.sh
@@ -6,8 +6,10 @@ docker stop mongo
 docker stop schueler
 docker system prune --force 
 
+docker volume create mongoVolume
+
 docker load -i Schuelerverwaltung.tar
 docker network create --driver=bridge --gateway 172.4.0.1 --subnet=172.4.0.0/24 schuelernetz
 
-docker run -d --net schuelernetz --ip 172.4.0.3 -e MONGO_INITDB_DATABASE=test --name mongo --restart on-failure -v /data/db mongo:latest
+docker run -d --net schuelernetz --ip 172.4.0.3 -e MONGO_INITDB_DATABASE=test --name mongo --restart on-failure -v mongoVolume:/data/db mongo:latest
 docker run -d -p 443:8443 --net schuelernetz --ip 172.4.0.2 --restart on-failure -v /usr/src/app/src/ssl --name schueler schueler
